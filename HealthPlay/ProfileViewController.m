9d5b2ac117a2c4573e94746f1cea4bdc1d3e714a
YangShun <mr.tay.yang.shun@gmail.com>
2012-10-06 19:36:13 -0700

    Added images

--- HealthPlay/ProfileViewController.m
+++ HealthPlay/ProfileViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ProfileViewController.h"
+#import "NavigationViewController.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () {
+  IBOutlet UILabel *nameLabel;
+  IBOutlet UIScrollView *scrollView;
+  UIImage *boy1;
+  UIImage *boy2;
+  UIImage *girl1;
+  UIImage *girl2;
+  NSArray *avatarImages;
+  int avatarIndex;
+  NSMutableArray *avatarImageViews;
+  
+}
 
 @end
 
@@ -18,15 +30,51 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+      boy1 = [UIImage imageNamed:@"boy-1.png"];
+      boy2 = [UIImage imageNamed:@"boy-2.png"];
+      girl1 = [UIImage imageNamed:@"girl-1.png"];
+      girl2 = [UIImage imageNamed:@"girl-2.png"];
+      avatarImages = [NSArray arrayWithObjects:boy1, boy2, girl1, girl2, nil];
+      avatarImageViews = [NSMutableArray new];
     }
     return self;
 }
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+  [super viewDidLoad];
+  CGFloat curXLoc = 0;
+  for (UIImage *img in avatarImages) {
+    UIImageView *imgView = [[UIImageView alloc] initWithImage:img];
+    [avatarImageViews addObject:imgView];
+    [scrollView addSubview:imgView];
+    imgView.frame = CGRectMake(curXLoc, 0, 106, 200);
+    curXLoc += 106;
+  }
+  scrollView.delegate = self;
+  scrollView.pagingEnabled = YES;
+  scrollView.contentSize = CGSizeMake(curXLoc, 200);
+  // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  nameLabel.text = ((NavigationViewController*)(self.navigationController)).username;
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView
+                  willDecelerate:(BOOL)decelerate {
+  int page = floor((scrollView.contentOffset.x - 106 / 2) / 106) + 1;
+//  NSLog(@"%d", page);
+//  CGRect frame;
+//	frame.origin.x = scrollView.frame.size.width * page;
+//	frame.origin.y = 0;
+//	frame.size = scrollView.frame.size;
+//	[scrollView scrollRectToVisible:frame animated:YES];
+}
+
+- (IBAction)back:(id)sender {
+  [self.navigationController popViewControllerAnimated:YES];
 }
 
 - (void)viewDidUnload
